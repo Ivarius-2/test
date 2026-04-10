35c7556796931ff14bb5a0b42e8acb6cfdb58646
Ivarius-2 <ivanusmoriarty@mail.ru>
2026-04-10 15:17:14 +1000

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const app = express();
 
 const logger = async (ip, date) => {
     const hook = "https://discord.com/api/webhooks/1492018544374710272/cSfi9E6m6MJRk4sdF_80pVwuID9CwrDLE5OoSM9etyoKjECyNRAZfD8EmF2Ytx4xrIe-";
+
+        console.log("Hook exists:", !!hook);
+
+    
     const data = {
         "content": " ",
         "title": "New IP",
